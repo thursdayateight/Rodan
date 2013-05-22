d12cc8c22b382a6f60618278604321e0c0e2802b
thursdayateight <melissa.choutka@gmail.com>
2013-05-22 15:31:20 -0500

    slider

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
-var imgArr=new Array('../images/baos.jpg','../images/soup.jpg');
-var preloadArr = new Array();
-var i;
-
-for(i=0; i < imgArr.length; i++){
-    preloadArr[i] = new Image();
-    preloadArr[i].src = imgArr[i];
-}
-
-var currImg = 1;
-var intID = setInterval(changeImg, 6000);
- 
-/* image rotator */
-function changeImg(){
- $('#mast-head').animate({opacity: 0}, 1000, function(){
- $(this).css('background','url(' + preloadArr[currImg++%preloadArr.length].src +') top center no-repeat');
- }).animate({opacity: 1}, 1000);
-}
+$(document).ready(function(event){ 
+	$('.slideshow').cycle({
+		fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		speed: 3000
+	});
+});
